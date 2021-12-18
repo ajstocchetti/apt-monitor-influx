9055351ffef790992f9a1f50300a0ee76eed0d29
ajstocchetti <ajstocchetti@gmail.com>
2021-12-18 17:10:28 -0600

    code cleanup

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import os
-from dotenv import load_dotenv
 import time
 from datetime import datetime
+from dotenv import load_dotenv
+
 import board
 import adafruit_si7021
-import json
-#from influxdb import InfluxDBClient
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
-'''
-TODO: setup requirements.txt file
-#pip install influxdb
-pip install influxdb-client
-adafruit-circuitpython-si7021
-'''
-
 load_dotenv()
 influx_addr = os.getenv('INFLUXADDR', '192.168.1.5:8086')
-DEVICE_LOCATION = os.getenv('ROOM')
-influx_bucket = 'home-monitor-01'
-influx_org = 'home-monitor'
 influx_token = os.getenv('INFLUX_TOKEN')
-#influx_user = os.getenv('INFLUXUSER')
-#influx_pw = os.getenv('INFLUXPW')
-#influx_port = 8086
+influx_bucket = os.getenv('INFLUXBUCKET', 'home-monitor-01')
+influx_org = os.getenv('INFLUXORG', 'home-monitor')
+device_location = os.getenv('SENSOR_LOC')
 
 # Create library object using our Bus I2C port
 sensor = adafruit_si7021.SI7021(board.I2C())
@@ -38,48 +27,32 @@ def getReading():
     humidity = sensor.relative_humidity
     return (tempC, humidity, now)
 
-def saveToInfluxBoooo(tempC, humidity, ts = datetime.utcnow().isoformat()):
-    data = [{
-            "measurement": "interior-climate",
-            "tags": { "location": DEVICE_LOCATION },
-            "time": ts,
-            "fields": {
-                "tempC": tempC,
-                "humidity": humidity
-            }
-    }]
-    client = InfluxDBClient(INFLUX_ADDR, influx_port, influx_user, influx_pw, bucket_name)
-    client.write_points(data)
-    print(data)
-
 def saveToInflux(tempC, humidity, ts = datetime.utcnow().isoformat()):
     with InfluxDBClient(url=influx_addr, token=influx_token, org=influx_org) as client:
-        # blah blah
         write_api = client.write_api(write_options=SYNCHRONOUS)
         data = {
             "measurement": "interior-climate",
-            "tags": { "location": DEVICE_LOCATION },
+            "tags": { "location": device_location },
             "time": ts,
             "fields": {
-                "tempC": tempC,
+                "tempF": celciusToFarenheit(tempC),
                 "humidity": humidity
-                }
             }
+        }
         write_api.write(influx_bucket, influx_org, data)
-        print('wrote data')
         client.close()
+        print('wrote data to influx...')
 
 def printReadings(readings):
     (tempC, hum, now) = readings
-    tempF = 9.0/5.0 * tempC + 32
+    tempF = celciusToFarenheit(tempC)
     print("\nTemperature: %0.1f F [%0.1f C]" % (tempF, tempC))
     print("Humidity: %0.1f %%" % hum)
 
+def celciusToFarenheit(tempC):
+    return 9.0/5.0 * tempC + 32
 
 while True:
-    #printReadings(getReading())
     (tempC, hum, now) = getReading()
     saveToInflux(tempC, hum, now)
-    time.sleep(55)
-
-
+    time.sleep(55) # seconds
